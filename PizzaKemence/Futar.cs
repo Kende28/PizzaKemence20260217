887a9cf28103a5f1596f1ef611d19432e11b2ee3
Kende28 <yamakawakende676@gmail.com>
2026-02-17 13:21:48 +0100

    Futar Commit

--- PizzaKemence/Futar.cs
+++ PizzaKemence/Futar.cs
@@ -9,5 +9,20 @@ namespace PizzaKemence
 	internal class Futar
 	{
 		private PizzaKemence pizzaKemence;
+
+		public Futar(PizzaKemence pizzaKemence) 
+		{
+			this.pizzaKemence = pizzaKemence;
+		}
+
+		public void KiveszPizza()
+		{
+			while (true)
+			{
+				Pizza pizza = pizzaKemence.KiveszPizza();
+				Console.WriteLine($"Futár szállította: {pizza}");
+				Thread.Sleep(pizza.Meret * 100);
+			}
+		}
 	}
 }
--- PizzaKemence/Program.cs
+++ PizzaKemence/Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main(string[] args)
         {
+            PizzaKemence pizzaKemence = new PizzaKemence(5);
+
+            Sef sef = new Sef(pizzaKemence);
+            Futar futar = new Futar(pizzaKemence);
+
             
         }
     }
