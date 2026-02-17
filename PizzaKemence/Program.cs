f02af023d7296193412818aa261d8e3a3cd5411c
Kende28 <yamakawakende676@gmail.com>
2026-02-17 13:28:10 +0100

    Program Commit

--- PizzaKemence/Program.cs
+++ PizzaKemence/Program.cs
@@ -7,9 +7,15 @@
             PizzaKemence pizzaKemence = new PizzaKemence(5);
 
             Sef sef = new Sef(pizzaKemence);
-            Futar futar = new Futar(pizzaKemence);
+			Futar futar = new Futar(pizzaKemence);
 
-            
-        }
+            Thread sefSzal1 = new Thread(sef.FeladPizza);
+			Thread sefSzal2 = new Thread(sef.FeladPizza);
+			Thread futarSzal = new Thread(futar.KiveszPizza);
+
+            sefSzal1.Start();
+			sefSzal2.Start();
+            futarSzal.Start();
+		}
     }
 }
